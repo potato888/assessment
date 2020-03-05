dfbdcf638f2f1bd1779eb0871432fcb73f0b63e0
potato888 <46345907+potato888@users.noreply.github.com>
2020-03-05 22:55:52 +0900

    #1 に対する対応を行いました。

--- assessment.js
+++ assessment.js
@@ -52,6 +52,7 @@
   };
 
   const answers = [
+    　'{userName}のいいところは人柄です。{userName}は良い人です。',　
       '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
       '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
       '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
